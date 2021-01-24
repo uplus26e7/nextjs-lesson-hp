63893fe352308af06856318270da2c7ab7660608
Yuya Shintani <yuya.shintani.uplus26e7@gmail.com>
2021-01-24 09:53:09 +0900

    🎉 First Commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen py-0 px-2 flex flex-col justify-center items-center">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Hello Next.js</h1>
+    </div>
+  );
+}
